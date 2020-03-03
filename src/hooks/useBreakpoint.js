02f324f217f0fcb46d8e91442d29c328493e71a2
Destin Raymundo <draym001@g.ucla.edu>
2020-03-02 20:04:46 -0800

    fixed useBreakpoint

--- src/hooks/useBreakpoint.js
+++ src/hooks/useBreakpoint.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useLayoutEffect } from "react"
+import { useState, useEffect } from "react"
 import throttle from "src/utils/throttle"
 
 const getSizeFromWidth = width => {
@@ -19,36 +19,14 @@ export default () => {
   const [size, setSize] = useState(
     getSizeFromWidth(typeof window !== `undefined` ? window.innerWidth : 321)
   )
-  useLayoutEffect(() => {
-    console.log("LAYOUT EFFECT RAN")
-    console.log(
-      "LAYOUT",
-      window.innerWidth,
-      getSizeFromWidth(window.innerWidth)
-    )
-    setSize(
-      getSizeFromWidth(typeof window !== `undefined` && window.innerWidth)
-    )
-  }, [])
   useEffect(() => {
-    console.log("EFFECT RAN")
-    console.log(
-      "EFFECT",
-      window.innerWidth,
-      getSizeFromWidth(window.innerWidth)
-    )
-    setSize(
-      getSizeFromWidth(typeof window !== `undefined` && window.innerWidth)
-    )
+    setSize(getSizeFromWidth(window.innerWidth))
     const calcSize = throttle(() => {
-      setSize(
-        getSizeFromWidth(typeof window !== `undefined` && window.innerWidth)
-      )
+      setSize(getSizeFromWidth(window.innerWidth))
     })
-    typeof window !== `undefined` && window.addEventListener("resize", calcSize)
+    window.addEventListener("resize", calcSize)
     return () => {
-      typeof window !== `undefined` &&
-        window.removeEventListener("resize", calcSize)
+      window.removeEventListener("resize", calcSize)
     }
   }, [])
   return { breakpoint: size }
